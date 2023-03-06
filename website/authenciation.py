24a1c400317cda78781a9499e0fa72394b4432de
jnizam <jaernizam2@gmail.com>
2023-03-05 20:15:12 -0600

    started working on form module

--- website/authenciation.py
+++ website/authenciation.py
@@ -21,7 +21,6 @@ def register():
         password2 = request.form.get('password2')
         
 
-
         if len(username) < 5:
             flash('Username must be greater than 5 characters.', category='error')
         elif password1 != password2:
@@ -30,11 +29,26 @@ def register():
             flash('Password must be greater than 5 characters.', category='error')
         else:
             flash('Account creation successful', category='success')
-        print(request.form)
+
     return render_template("register.html")
 
 @authenciator.route('/form', methods=['GET','POST'])
 def form():
+    if request.method == 'POST':
+        numGallons = request.form.get('gallonsReq')
+        address = request.form.get('deliveryAddr')
+        date = request.form.get('deliveryDate')
+        
+        #calculate suggest price with pricing module which will be imported later.
+        #calculate total amount after pricing module.
+
+        try:
+            num = int(x)
+            print("You entered the integer:")
+        except ValueError:
+            print("You did not enter a valid integer")
+
+
     return render_template("form.html")
 
 @authenciator.route('/complete', methods=['GET', 'POST'])
