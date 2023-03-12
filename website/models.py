0ee980e4087e94ff1ac16c2801a58ee23308615a
Minho Kim <music4537@naver.com>
2023-03-12 17:57:12 -0500

    feat : created user_login modue & created state/zipcode on quote form

--- website/models.py
+++ website/models.py
@@ -2,27 +2,32 @@ from . import db #database from __init__.py
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class User(db.model, UserMixin):
+class User(db.model):
     id = db.column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-
-
-class User_info(db.model):
-    id = db.column(db.Integer, primary_key=True)
     fullName = db.Column(db.String(50))
     Address1 = db.Column(db.String(100))
     Address2 = db.Column(db.String(100))
     City = db.Column(db.String(100))
     State = db.Column(db.String(2))
     Zip = db.Column(db.String(9))
+    quotes = db.relationship('Fuel_Quote')
+
+class User_login(db.model, UserMixin):
+    id = db.column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
 
 class Fuel_Quote(db.model):
-    id = db.column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     gallons_req = db.Column(db.Integer(150))
     delivery_date = db.Column(db.DateTime(timezone=True), default=func.now())
-    delivery_state = db.Column(db.String(100))
-    delivery_address = db.Column(db.String(100))
+    delivery_address1 = db.Column(db.String(100))
+    delivery_address2 = db.Column(db.String(100))
+    delivery_city = db.Column(db.String(100))
+    delivery_state = db.Column(db.String(2))
+    delivery_zipcode = db.Column(db.Integer(5))
     suggested_price = db.Column(db.Integer(150))
     total_amount = db.Column(db.Integer(150))
 
