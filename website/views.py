748d20a6fb83876f3d4e689b99ad08887ad48e46
Minho Kim <music4537@naver.com>
2023-03-13 17:34:31 -0500

    feat : delete function implemented on quote list

--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify, flash, redirect, url_for
 from flask_login import login_required, current_user
+from .models import FuelQuote, User
+from . import db  # database from __init__.py
+import json
+
 
 viewer = Blueprint('viewer', __name__)
 
@@ -13,3 +17,16 @@ def home():
 @login_required
 def history():
     return render_template("history.html", user=current_user)
+
+
+@viewer.route('/delete-quote', methods=['POST'])
+def delete_quote():
+    quote = json.loads(request.data)
+    print(quote)
+    quote_no = quote['quote_no']
+    quote = FuelQuote.query.get(quote_no)
+    if (quote):
+        if quote.user_id == current_user.id:
+            db.session.delete(quote)
+            db.session.commit()
+            return jsonify({})
